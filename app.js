ef52289f946cf7bcfe938b934a07862eb4a9c194
“First <michael.s.carlson@gmail.com>
2016-03-03 11:34:11 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+//console.log("Off to a good start?");
+  $(".button").on("click", appendContainer);
+  $(".container").on("click");
+});
+
+function appendContainer(){
+
+
+
+}
